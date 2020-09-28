04f599ec02e440dae4f4af708e4873aca5a9b9bb
Nathan Isaac <nathanjisaac@gmail.com>
2020-09-27 22:27:46 -0700

    Switch to table tests

--- parser_test.go
+++ parser_test.go
@@ -6,77 +6,89 @@ import (
 	"testing"
 )
 
-func Test_parsing_tag(t *testing.T) {
-	t.Run("empty string", func(t *testing.T) {
-		handler := strings.NewReader("")
-		parser := CreateParser()
-		parser.parse(handler)
-
-		assert.Equal(t, []Tag{}, parser.tags)
-	})
-
-	t.Run("tags name", func(t *testing.T) {
-		handler := strings.NewReader(`
+func TestParsingTags(t *testing.T) {
+	var tagTests = []struct {
+		name  string
+		in  string
+		out []Tag
+	}{
+		{
+			"empty string",
+			"",
+			[]Tag{},
+		},
+		{
+			"tag with name",
+			`
 !Type:Tag
 NExample Name
 ^
-`)
-		parser := CreateParser()
-		parser.parse(handler)
-
-		assert.Equal(t, []Tag{{
-			name: "Example Name",
-		}}, parser.tags)
-	})
-
-	t.Run("tags description", func(t *testing.T) {
-		handler := strings.NewReader(`
+`,
+		[]Tag{{
+				name: "Example Name",
+			}},
+		},
+		{
+			"tag with description",
+			`
 !Type:Tag
 DExample Description
 ^
-`)
-		parser := CreateParser()
-		parser.parse(handler)
-
-		assert.Equal(t, []Tag{{
-			description: "Example Description",
-		}}, parser.tags)
-	})
-
-	t.Run("tag with name and description", func(t *testing.T) {
-		handler := strings.NewReader(`
+`,
+		[]Tag{{
+				description: "Example Description",
+			}},
+		},
+		{
+			"tag with description",
+			`
+!Type:Tag
+DExample Description
+^
+`,
+			[]Tag{{
+				description: "Example Description",
+			}},
+		},
+		{
+			"tag with name and description",
+			`
 !Type:Tag
 NExample Name
 DExample Description
 ^
-`)
-		parser := CreateParser()
-		parser.parse(handler)
-
-		assert.Equal(t, []Tag{{
-			name: "Example Name",
-			description: "Example Description",
-		}}, parser.tags)
-	})
-
-	t.Run("multiple tags", func(t *testing.T) {
-		handler := strings.NewReader(`
+`,
+			[]Tag{{
+				name: "Example Name",
+				description: "Example Description",
+			}},
+		},
+		{
+			"multiple tags",
+			`
 !Type:Tag
 NTag 1
 ^
 NTag 2
 DTag 2 Description
 ^
-`)
-		parser := CreateParser()
-		parser.parse(handler)
+`,
+			[]Tag{{
+				name: "Tag 1",
+			}, {
+				name: "Tag 2",
+				description: "Tag 2 Description",
+			}},
+		},
+	}
 
-		assert.Equal(t, []Tag{{
-			name: "Tag 1",
-		}, {
-			name: "Tag 2",
-			description: "Tag 2 Description",
-		}}, parser.tags)
-	})
+	for _, tt := range tagTests {
+		t.Run(tt.name, func(t *testing.T) {
+			handler := strings.NewReader(tt.in)
+			parser := CreateParser()
+			parser.parse(handler)
 
+			assert.Equal(t, tt.out, parser.tags)
+		})
+	}
 }
